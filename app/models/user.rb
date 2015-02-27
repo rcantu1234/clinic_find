44c5a1de5de684356e1e059094ba287080b6def8
ROEL CANTU <roel.cantu@yahoo.com>
2015-02-27 13:09:33 -0600

    Validating attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates_presence_of :first_name, :last_name,
+                        :age, :gender, :height, :weight, :diseases, :allergies
 
 end
 
