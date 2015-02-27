90d7e97f322b24fa914e2a4cc751153bca3ec14e
Charles Lueker <charleslueker@yahoo.com>
2015-02-27 16:16:34 -0600

    angular files built and ready for divs and styling

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,1 +1,20 @@
-
+angular.module('app', ['app.controllers','ui.router'])
+.config(function($stateProvider,$urlRouterProvider){
+	$stateProvider
+	.state('home',{
+		url: '/',
+		templateUrl: 'templates/home.html',
+		controller: 'HomeCtrl'
+	})
+	.state('clinicMap',{
+		url:'/clinicMap',
+		templateUrl: 'templates/clinicMap.html',
+		controller: 'ClinicCtrl'
+	})
+	.state('Appointments',{
+		url: '/Appointments',
+		templateUrl: 'templates/Appointments',
+		controller: 'AppointCtrl'
+	});
+	$urlRouterProvider.otherwise("/");
+});
--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -1,1 +1,15 @@
+angular.module('app.controllers',[])
+.controller('HomeCtrl', function($scope,$http,$window){
+	console.log('home control');
 
+})
+
+.controller('ClinicCtrl', function($scope,$http,$window){
+	console.log('clinic control');
+
+})
+
+.controller('AppointCtrl', function($scope,$http,$window){
+	console.log('appoint control');
+	
+});
